ce889038082708dff2fb5287b7cb475451682b23
Olisehgenesis <61495902+Olisehgenesis@users.noreply.github.com>
2023-07-29 05:51:49 +0300

    fully

--- app_service.tf
+++ app_service.tf
@@ -2,27 +2,23 @@ locals {
   operating_system_local_app_service = "Windows"  # Replace "Windows" with the appropriate operating system value
 }
 
-module "app_service" {
-  #source = "https://dev.azure.com/NAFTechnologyServices/Cloud%20Architecture%20Templates/_git/modules?path=/app_service.tf"
-  # source = "github.com/Olisehgenesis/modules"
-  operating_system = local.operating_system_local_app_service
+operating_system = local.operating_system_local_app_service
 
-  site_config {
-    always_on                 = true
-    use_32_bit_worker_process = true
-    websockets_enabled        = true
-  }
+site_config {
+  always_on                 = true
+  use_32_bit_worker_process = true
+  websockets_enabled        = true
+}
 
-  tags = {
-    Owner     = var.tag_owner
-    Department = var.tag_department
-  }
+tags = {
+  Owner     = var.tag_owner
+  Department = var.tag_department
+}
 
-  # Choose the Operating System based on the local variable value
-  os_type = local.operating_system_local_app_service
+# Choose the Operating System based on the local variable value
+os_type = local.operating_system_local_app_service
 
-  # Enable Application Insights
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insights.instrumentation_key
-  }
+# Enable Application Insights
+app_settings = {
+  "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insights.instrumentation_key
 }
